d7006bd6ce54295c4e2a31d19944c17086971ec9
Tianyi Zhang <54869377+TianyiZhang-Timothy@users.noreply.github.com>
2020-04-21 05:39:04 -0400

    use findplacefromtext api (from googlemap) in map.php

--- map.php
+++ map.php
@@ -12,29 +12,50 @@
 </section>
 
 <section id="main_map">
+
+    <div id="map"></div>
+    <script>
+        var map;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: -34.397, lng: 150.644},
+                zoom: 8
+            });
+        }
+    </script>
+    <!--    <script src="https://maps.googleapis.com/maps/api/js?key=-->
+    <? //= $GMP_API_KEY; ?><!--<!--&callback=initMap"-->
+    <!--            async defer></script>-->
+
     <?php
+    //    https://maps.googleapis.com/maps/api/place/findplacefromtext/json?
+    //input=Museum%20of%20Contemporary%20Art%20Australia&
+    //inputtype=textquery&
+    //fields=photos,formatted_address,name,rating,opening_hours,geometry&
+    //key=YOUR_API_KEY
     // get parameters
-    if(isset($_GET['address'])){
+    if (isset($_GET['address'])) {
 //        echo $_GET['address'];
-    }else {
+        // keys
+        require_once "keys/keys.php";
+        // send get request
+        require_once "includes/http_functions.php";
+        $res = _httpGet("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" .
+            "input=" . $_GET['address'] .
+//            "&inputtype=textquery&" .
+//            "fields=photos,formatted_address,name,rating,opening_hours,geometry&" .
+            "key=" . $GMP_API_KEY);
+//        var_dump($res);
+        ?>
+        <script src="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=<?= $GMP_API_KEY; ?>&input=<?= $_GET['address']; ?>" async defer></script>
+
+        <?php
+
+    } else {
 //        echo "nope";
     }
     ?>
-
-<!--        <div id="map"></div>-->
-<!--        <script>-->
-<!--            var map;-->
-<!---->
-<!--            function initMap() {-->
-<!--                map = new google.maps.Map(document.getElementById('map'), {-->
-<!--                    center: {lat: -34.397, lng: 150.644},-->
-<!--                    zoom: 8-->
-<!--                });-->
-<!--            }-->
-<!--        </script>-->
-<!--        <script src="https://maps.googleapis.com/maps/api/js?key=--><?//= $GMP_API_KEY; ?><!--&callback=initMap"-->
-<!--                async defer></script>-->
-
 
 </section>
 
